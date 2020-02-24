157e864e3a7c3575448e98e58aaa5b774be155f8
Onar <onardejesus@gmail.com>
2020-02-24 08:24:49 +0800

    added(component): common InputText component

---
+++ src/common/InputText.js
@@ -0,0 +1,41 @@
+import * as React from 'react'
+import {View, StyleSheet, TextInput, Text} from 'react-native'
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        marginVertical: 5,
+    },
+    input: {
+        height: 40, 
+        borderColor: 'gray', 
+        borderWidth: 0.5,
+        borderRadius: 5
+    },
+    error: {
+        color: 'red',
+        fontSize: 10
+    }
+})
+
+interface Props {
+    onChangeText: (text: string) => void;
+    placeholder: string;
+    value: string;
+    hasError?: Boolean;
+}
+
+const InputText = ({onChangeText, placeholder, value, hasError}: Props) => {
+    return (
+        <View style={styles.inputContainer}>
+            <TextInput
+                placeholder={placeholder}
+                onChangeText={onChangeText}
+                style={styles.input}
+                value={value}
+            />
+            {!hasError || <Text style={styles.error}>Please complete the form</Text>}
+        </View>
+    )
+}
+
+export default InputText
